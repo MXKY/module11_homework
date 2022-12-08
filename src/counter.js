20f1412e16751c23f95f33574aad6322fa328935
MXKY <swear1997@yandex.ru>
2022-12-09 02:09:18 +0300

    Integer? modified

--- src/counter.js
+++ src/counter.js
@@ -4,7 +4,10 @@ export function countDown(number) {
 
     if (number > 100)
         return "Число не может быть больше ста.";
-        
+
+    if (!Number.isInteger(number))
+        return "Ожидается целое число.";
+
     let answer = String();
 
     for (let i = number; i >= 1; i--)
